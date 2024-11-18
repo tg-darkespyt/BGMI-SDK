d5c306d1b6d0fc8bf5060e82f904275f426f936f
vigneshperiasamy <darkespyt8577@gmail.com>
2024-11-18 19:24:09 +0530

    Initial Commit

---
+++ x32/Offset.h
@@ -0,0 +1,12 @@
+#define GEngine_Offset 0x870e9d0
+#define GNames_Offset 0x4372A40
+#define GUObject_Offset 0x8A5BF78
+#define GNativeAndroidApp_Offset 0x8897F68
+#define Actors_Offset 0x5E83568
+#define ViewMatrix_Offset 0x8C12FBC
+#define ProcessEvent 0x5A182DC
+#define CanvasMap_Offsets 0x88985f8
+#define Actors_Offset 0x70
+//JOIN HERE : @DARKESPYT
+//JOIN HERE : @DARKESPYT
+//JOIN HERE : @DARKESPYT
